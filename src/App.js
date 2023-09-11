095683336d99b1851c8ecd70bc54b2b607728b18
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2023-09-11 22:52:54 +0800

    my-first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import {  useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import Card from './Card';
+
+
+import SearchIcon from './search.svg';
+
+// b7ee9859 -> API key
+
+const API_URL = "http://www.omdbapi.com?apikey=b7ee9859"
+
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]) ;
+
+    const [search, setSearch] = useState('');
+
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+ 
+
+    useEffect( () => {
+        searchMovies("Batman")
+    }, [] )
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search For Movies" 
+                value={search} 
+                onChange={ (value) => {
+                    setSearch(value.target.value)
+                }}/>
+
+                <img 
+                    src={SearchIcon}
+                    alt="Search"
+                    onClick={ () => searchMovies(search) }
+                />
+            </div>
+
+
+                { movies?.length > 0 
+                    ?   (
+
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <Card movie={movie} />
+                            ))}
+                        </div>
+                    ) :
+
+                    (
+                        <div className="empty">
+                            <h3>No Movies Found</h3>
+                        </div>
+                    ) 
+                
+                }
+                    
+            </div>
+
+            
+           
+   
+        
+    )
 }
 
-export default App;
+export default App; 
